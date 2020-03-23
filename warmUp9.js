1d98b594a1be2c47643442b192b76038380f4c65
MohamedAmine-Oueslati <wesamin1991@gmail.com>
2020-03-23 10:10:22 +0100

    warmUp9 done

---
+++ warmUp9.js
@@ -0,0 +1,50 @@
+// Style your code and explain it step by step before jumping to code :D
+// Read carefully, focus and test your code :) ... /\ HaPPy HaCkInG /\
+
+//1. Write a JavaScript function to check whether an `input` is an array or not
+// ex isArray([1,2])=>true
+//    isArray(5)=>false
+// i used the isArray method to determine whether the passed value is an Array or not
+function isArray(arr) {
+	return (Array.isArray(arr) );
+}
+
+// **************** git status ****************
+// **************** git add fileName.js ****************
+// **************** git status ****************
+// **************** git commit -m "your message" ****************
+// **************** git status ****************
+// **************** git push origin master ****************
+
+//2-Write a JavaScript function to get the first n element of an array.
+//ex  first([1,2,3],1)=>[1]
+//    first([1,2,3,4]2)=>[1,2]
+// i used a for loop for my new array to put every element in the passed array to my new array until i reach the index num-1
+function first (array,num) {
+	var arr = [] ;
+	for(var i = 0 ; i < num ; i++) {
+		arr[i] = array[i] ;
+	}
+	return arr ;
+}
+
+// **************** git status ****************
+// **************** git add fileName.js ****************
+// **************** git status ****************
+// **************** git commit -m "your message" ****************
+// **************** git status ****************
+// **************** git push origin master ****************
+
+//3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
+//ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+// i used the Object.entries method to put every key and his value in a new array
+function convert(obj) {
+	var arr = Object.entries(obj)
+	return arr;
+}
+// **************** git status ****************
+// **************** git add fileName.js ****************
+// **************** git status ****************
+// **************** git commit -m "your message" ****************
+// **************** git status ****************
+// **************** git push origin master ****************
